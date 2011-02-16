19a5e954afd76d9889ded96ed1e3cb29dc35d495
Louis Wasserman <wasserman.louis@gmail.com>
2011-02-16 12:21:33 -0600

    Modified handwritten ByteString.toRep to work for 64-bit words

--- Data/TrieMap/Representation/Instances/ByteString.hs
+++ Data/TrieMap/Representation/Instances/ByteString.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE UndecidableInstances, TypeFamilies, BangPatterns #-}
+{-# LANGUAGE UndecidableInstances, TypeFamilies, BangPatterns, CPP #-}
 module Data.TrieMap.Representation.Instances.ByteString () where
+
+#include "MachDeps.h"
 
 import Data.TrieMap.Representation.Class
 import Data.TrieMap.Utils
@@ -10,6 +12,7 @@ import Data.Primitive.ByteArray
 import Foreign.Ptr
 import Foreign.Storable
 import Foreign.ForeignPtr
+import Foreign.Marshal.Array
 
 import Data.Bits
 import Data.Word
@@ -31,28 +34,55 @@ instance Repr ByteString where
 
 bsToRep :: ByteString -> Vector Word
 bsToRep (PS fp off n) = if n <= 0 then empty else inlinePerformIO $ withForeignPtr fp $ \ p0 -> 
-  let !src = p0 `plusPtr` off :: Ptr Word8 in do
-    !dest <- newByteArray (n' * sizeOf (0 :: Word))
-    let go !i = if ii < n' then (do
-	  w <- accum 3 0 $ accum 2 8 $ accum 1 16 $ accum 0 24 $ return 0
-	  out w
-	  go ii) else do
-	  let w0 = accum 0 24 (return 0)
-	      w1 = accum 1 16 w0
-	      w2 = accum 2 8 w1
-	      w3 = accum 3 0 w2
-	  case n .&. 3 of
-	    1	-> out =<< w0
-	    2	-> out =<< w1
-	    3	-> out =<< w2
-	    _	-> out =<< w3
-	  where !ii = i + 1; !i' = i `shiftL` 2
-		{-# INLINE accum #-}
-		accum x s w = liftM2 (.|.) w $ liftM (\ w -> fromIntegral w .<<. s) $ peekElemOff src (x + i')
+  let !src = p0 `advancePtr` off :: Ptr Word8 in do
+    !dest <- newByteArray (n' * bytesPerWord)
+    let go !i = if ii < n' then (readWordAt src i >>= out >> go ii) else readLastWordAt n i src >>= out
+	  where !ii = i + 1
 		out = writeByteArray dest i
     go 0
     unsafeFreeze (MVector 0 n' dest)
-  where n' = (n + 3) `shiftR` 2
+  where n' = (n + (bytesPerWord - 1)) `quoPow` bytesPerWord
+
+bytesPerWord :: Int
+bytesPerWord = sizeOf (0 :: Word)
+
+readWordAt :: Ptr Word8 -> Int -> IO Word
+readWordAt ptr off = 
+#if WORD_SIZE_IN_BITS == 32
+  accum 3 0 $ accum 2 8 $ accum 1 16 $ accum 0 24 $ return 0
+#else
+  accum 7 0 $ accum 6 8 $ accum 5 16 $ accum 4 24 $ accum 3 32 $ accum 2 40 $ accum 1 48 $ accum 0 56
+#endif
+  where !off' = off * bytesPerWord
+	accum x s w = liftM2 (.|.) w $ liftM (\ w -> fromIntegral w .<<. s) $ peekElemOff ptr (x + off')
+
+readLastWordAt :: Int -> Int -> Ptr Word8 -> IO Word
+readLastWordAt !n !off !ptr =
+  let	w0 = accum 0 (return 0)
+	w1 = accum 1 w0
+	w2 = accum 2 w1
+	w3 = accum 3 w2
+	w4 = accum 4 w3
+	w5 = accum 5 w4
+	w6 = accum 6 w5
+	w7 = accum 7 w6
+    in case n `remPow` bytesPerWord of
+      1	-> w0
+      2	-> w1
+      3	-> w2
+#if WORD_SIZE_IN_BITS > 32
+      4	-> w3
+      5	-> w4
+      6	-> w5
+      7	-> w6
+      _	-> w7
+#else
+      _	-> w3
+#endif
+  where	!off' = off * bytesPerWord
+	{-# INLINE accum #-}
+	accum x w = let s = 8 * (bytesPerWord - 1 - x) in
+	  liftM2 (.|.) w $ liftM (\ w -> fromIntegral w .<<. s) $ peekElemOff ptr (x + off')
 
 instance Repr L.ByteString where
 	type Rep L.ByteString = Rep ByteString
